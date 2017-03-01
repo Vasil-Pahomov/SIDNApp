9180023e7626515855100d3c59c8c486653a23de
Vasil <vp@clevertec.ru>
2017-03-01 20:43:43 +0300

    Рабочая триангуляция - тестовые данные

---
+++ app/src/main/java/lvr/sidnapp/Position.java
@@ -0,0 +1,40 @@
+package lvr.sidnapp;
+
+/**
+ * Created by Vasil on 27.02.2017.
+ */
+
+public class Position {
+    private double x, y, z;
+
+    public Position(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+}
+
--- app/src/main/java/lvr/sidnapp/TriangActivity.java
+++ app/src/main/java/lvr/sidnapp/TriangActivity.java
@@ -1,6 +1,11 @@
 package lvr.sidnapp;
 
 import android.os.Bundle;
+import android.renderscript.Double2;
+import android.util.Log;
+import android.view.View;
+
+import Jama.Matrix;
 
 public class TriangActivity extends BaseLocationActivity {
 
@@ -8,15 +13,34 @@ public class TriangActivity extends BaseLocationActivity {
 
     private final double SOUND_VELOCITY = 331;//meters per second
 
+    private final double roomWidth = 5, roomHeight = 5;
+
+
+    private Position beaconPositions[] = new Position[NSIG];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_triang);
 
         posView = (PositionView)findViewById(R.id.posView);
-        posView.setRoom(1.3, 3);
+        posView.setRoom(roomWidth, roomHeight);
 
-        startLocationUpdate();
+        beaconPositions[0] = new Position(0, 0, 1);
+        beaconPositions[1] = new Position(roomWidth, 0 ,1);
+        beaconPositions[2] = new Position(roomWidth, roomHeight, 1);
+        beaconPositions[3] = new Position(0, roomHeight, 1);
+
+        /*BeaconData[] data = new BeaconData[4];
+        data[0] = new BeaconData(0, (int) Math.round(1.1021 / SOUND_VELOCITY * RecordingThread.SAMPLE_RATE), 0);
+        data[1] = new BeaconData(1, (int) Math.round(1.1590 / SOUND_VELOCITY * RecordingThread.SAMPLE_RATE), 0);
+        data[2] = new BeaconData(2, (int) Math.round(1.1510 / SOUND_VELOCITY * RecordingThread.SAMPLE_RATE), 0);
+        data[3] = new BeaconData(3, (int) Math.round(1.0937 / SOUND_VELOCITY * RecordingThread.SAMPLE_RATE), 0);
+        Position pos = trilaterate(data, beaconPositions);
+        Log.d("SIDN", String.format("x=%f, y=%f", pos.getX(), pos.getY()));
+        */
+
+        //startLocationUpdate();
     }
 
     @Override
@@ -55,7 +79,87 @@ public class TriangActivity extends BaseLocationActivity {
 
     //convert sample index to distance in meters
     private double indToMeters(int index) {
-        return index / RecordingThread.SAMPLE_RATE * SOUND_VELOCITY;
+        return (double) index * SOUND_VELOCITY / RecordingThread.SAMPLE_RATE;
     }
 
+    private Position trilaterate(BeaconData[] data, Position[] pos)
+    {
+        double[][] aA = new double[NSIG][4];
+        double[] aR = new double[NSIG];
+        for (int i=0; i<NSIG; i++) {
+            int bi = data[i].getBeaconIndex();
+            aA[i][0] = pos[bi].getX();
+            aA[i][1] = pos[bi].getY();
+            aA[i][2] = pos[bi].getZ();
+
+            aA[i][3] = indToMeters(data[i].getPeakPosition());
+            aR[i] = 0.5 * Utils.LIP(aA[i], aA[i]);
+        }
+
+        Matrix  A = new Matrix(aA),
+                I0 = new Matrix(4,1,1),
+                R = new Matrix(aR, NSIG),
+
+                B = Utils.MatrixPinv(A),
+                u = B.times(I0),
+                v = B.times(R);
+
+        double  E = Utils.LIP(u,u),
+                F = Utils.LIP(u,v) - 1,
+                G = Utils.LIP(v,v),
+                l1 = (-2*F + Math.sqrt(4*F*F-4*E*G))/(2*E),
+                l2 = (-2*F - Math.sqrt(4*F*F-4*E*G))/(2*E);
+
+        if (Double.isNaN(l1)) {
+            //иногда получается, что квадратное уравнение не решается. Дискриминант оказывается слегка меньше нуля
+            //считаем, что это из-за погрешности и на самом деле он равен нулю
+            l1 = (-2*F)/(2*E);
+            l2 = l1;
+            Log.d("SIDN", "Approximation when trilaterating!!!");
+        }
+
+        Matrix  y1 = u.times(l1).plus(v),
+                y2 = v.times(l2).plus(v);
+
+        Position res = posFromMatrixIfMatches(y2);
+        if (res == null) {
+            res = posFromMatrixIfMatches(y1);
+        }
+
+        return res;
+
+    }
+
+    private Position posFromMatrixIfMatches(Matrix y) {
+        Position res = new Position(y.get(0,0), y.get(1,0), y.get(2,0));
+        if (res.getX() >= 0 && res.getX() <= roomWidth && res.getY() >= 0 && res.getY() <= roomHeight) {
+            return res;
+        } else {
+            return null;
+        }
+    }
+
+    public void posViewClick(View view) {
+        double x = 2.43, y = 2.51, z = 1.0;
+        BeaconData[] data = new BeaconData[NSIG];
+        double rad[] = new double[NSIG];
+        double corr = Math.random() / 10;
+        for (int i=0; i<NSIG; i++) {
+            rad[i] = Math.sqrt(
+                    (beaconPositions[i].getX() - x) * (beaconPositions[i].getX() - x)
+                            + (beaconPositions[i].getY() - y) * (beaconPositions[i].getY() - y)
+                            + (beaconPositions[i].getZ() - z) * (beaconPositions[i].getZ() - z)) + corr;
+
+            data[i] = new BeaconData(i, (int)
+                    Math.round(rad[i] / SOUND_VELOCITY * RecordingThread.SAMPLE_RATE), 0);
+        }
+
+        Position pos = trilaterate(data, beaconPositions);
+
+        if (pos == null) {
+            posView.setPos(0,0,rad[0], rad[1], rad[2], rad[3]);
+        } else {
+            posView.setPos(pos.getX(), pos.getY(), rad[0], rad[1], rad[2], rad[3]);
+        }
+    }
 }
